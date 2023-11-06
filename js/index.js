b31874fb1e2f3bdd4747ab95b4c302b2669f0214
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-11-06 13:21:32 -0500

    lateral movil

--- js/index.js
+++ js/index.js
@@ -15,4 +15,19 @@ function sigueme()
 
 
 }
+var toogle_menu_lateral=document.querySelector("#toogle_menu_lateral");
+//var toogle_menu=document.querySelector("#toogle_menu");
+var button_lateral=document.querySelector("#button_lateral")
+var menu=document.querySelector("#lateral")
+
+
+//toogle_menu.onclick= desplegar;
+toogle_menu_lateral.onclick=desplegar;
+
+function desplegar(){
+  
+
+menu.classList.toggle('active')
+
+}
 
